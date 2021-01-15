d56e74661b456a041be2ab6f32be5b0a7dc30bcd
Divyanshu Kumar <divyanshukumarg@gmail.com>
2021-01-15 19:17:43 +0530

    Replacing with new Golang code

---
+++ Golang-receiver/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/michaelbironneau/asbclient"
+)
+
+func main() {
+	log.Printf("Starting")
+
+	client := asbclient.New(asbclient.Queue, os.Getenv("sb_namespace"), os.Getenv("sb_key_name"), os.Getenv("sb_key_value"))
+
+	path := os.Getenv("sb_queue")
+
+	for {
+		log.Printf("Peeking...")
+		msg, err := client.PeekLockMessage(path, 30)
+
+		if err != nil {
+			log.Printf("Peek error: %s", err)
+		} else {
+			log.Printf("Peeked message: '%s'", string(msg.Body))
+			err = client.DeleteMessage(msg)
+			if err != nil {
+				log.Printf("Delete error: %s", err)
+			} else {
+				log.Printf("Deleted message")
+			}
+		}
+		rand.Seed(time.Now().UnixNano())
+		time.Sleep(time.Second * time.Duration(rand.Intn(40)))
+	}
+
+}
