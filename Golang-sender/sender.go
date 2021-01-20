6b8c879de4d12109e3369480e16cdd3f85b3677f
Divyanshu Kumar <divyanshukumarg@gmail.com>
2021-01-20 17:31:32 +0530

    Making sender slower by making it single thread

--- Golang-sender/sender.go
+++ Golang-sender/sender.go
@@ -7,36 +7,22 @@ import (
 
 	"github.com/michaelbironneau/asbclient"
 )
-var i int
-func sendMsg(max_msg int) {
+
+func main() {
+	log.Printf("Starting")
+	var maxMsg int
+	fmt.Printf("Enter number of messages to send\n")
+	fmt.Scanf("%d", &maxMsg)
 	client := asbclient.New(asbclient.Queue, os.Getenv("sb_namespace"), os.Getenv("sb_key_name"), os.Getenv("sb_key_value"))
 	path := os.Getenv("sb_queue")
-	for j := 0; j < max_msg; j++ {
-		i++
-		// log.Printf("Send: %d", i)
+	for j := 1; j <= maxMsg; j++ {
 		err := client.Send(path, &asbclient.Message{
-			Body: []byte(fmt.Sprintf("message %d", i)),
+			Body: []byte(fmt.Sprintf("message %d", j)),
 		})
 		if err != nil {
 			log.Printf("Send error: %s", err)
 		} else {
-			log.Printf("Sent: %d", i)
-		}
-	}
-}
-func main() {
-	log.Printf("Starting")
-	var max_msg int
-	fmt.Printf("Enter number of messages to send per thread, note we use 10 threads\n")
-	fmt.Scanf("%d", &max_msg)
-	i = 0;
-	for j:=0; j<9; j++ {
-		go sendMsg(max_msg)
-	}
-	sendMsg(max_msg)
-	for {
-		if i >=100 {
-			break
+			log.Printf("Sent: %d", j)
 		}
 	}
 }
